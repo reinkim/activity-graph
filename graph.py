d9478de0837acab3253530f124dcaa317b9bd302
Jinuk Kim <rein01@gmail.com>
2013-02-23 21:55:13 +0800

    Added summary for weekday

--- graph.py
+++ graph.py
@@ -53,7 +53,8 @@ def print_graph(output, stats, minDay, maxDay):
     firstDay = minDay - datetime.timedelta(days=minDay.weekday())
     skip = (7 - maxDay.weekday())
     lastDay = maxDay + datetime.timedelta(days=skip)
-    weekly = [0] * ((lastDay - firstDay).days / 7)
+    weekly = [0] * ((lastDay - firstDay).days / 7)  # per week
+    wdayly = [0] * 7  # per wday
 
     def _get_data(d):
         return stats.get(d, 0)
@@ -67,8 +68,10 @@ def print_graph(output, stats, minDay, maxDay):
 
     margin = 10
     # cell width = 10, intra-cell = 2, margin for text = 10
-    width = 12 * int((lastDay - firstDay).days / 7) - 2 + margin * 2 + 10
-    height = 12 * 7 - 2 + margin * 2 + 10
+    main_width = margin * 2 + 12 * int((lastDay - firstDay).days / 7) - 2 + 10
+    width = main_width + 40
+    main_height = 12 * 7 - 2 + margin * 2 + 10
+    height = main_height
     if FLAGS.weekly:
         height += 50 + 10
 
@@ -121,6 +124,7 @@ def print_graph(output, stats, minDay, maxDay):
                 print >> output, empty.format(i * 12, color)
             date += datetime.timedelta(days=1)
             weekly[column] += data
+            wdayly[i] += data
         column += 1
         print >> output, '  </g>'
     print >> output, '</g>'
@@ -130,33 +134,53 @@ def print_graph(output, stats, minDay, maxDay):
         c1 = colorsys.rgb_to_hls(0.1176, 0.4078, 0.1372)
         c2 = colorsys.rgb_to_hls(0.8392, 0.9019, 0.5215)
 
-        def _get_bar_color(v):
-            if v == 0:
-                return '#ffffff'
-            p = v / float(maxima)
-            q = 1.0 - p
-            c = (c1[0] * p + c2[0] * q, c1[1] * p + c2[1] * q,
-                 c1[2] * p + c2[2] * q)
-            c = colorsys.hls_to_rgb(*c)
-            c = tuple(int(_c * 255) for _c in c)
-            return '#%02x%02x%02x' % c
+        def _get_bar_color(_maxima):
+            maxima = _maxima
+            def _do_get_bar_color(v):
+                if v == 0:
+                    return '#ffffff'
+                p = v / float(maxima)
+                q = 1.0 - p
+                c = (c1[0] * p + c2[0] * q, c1[1] * p + c2[1] * q,
+                     c1[2] * p + c2[2] * q)
+                c = colorsys.hls_to_rgb(*c)
+                c = tuple(int(_c * 255) for _c in c)
+                return '#%02x%02x%02x' % c
+            return _do_get_bar_color
 
         bar = ('  <rect width="8" height="{height}" x="{x}" y="{y}" '
                'style="fill: {fill}"/>')
         print >> output, '<g transform="translate({0}, {1})">'.format(
                           margin + 10, height - 10 - 50)
+        get_color = _get_bar_color(maxima)
         for week in xrange(len(weekly)):
             if weekly[week] == 0:
                 continue
             bar_height = 50.0 * weekly[week] / maxima
             print >> output, bar.format(x=week * 12 + 2,
-                                        y=50 - bar_height,
+                                        y=0,
                                         height=bar_height,
-                                        fill=_get_bar_color(weekly[week]))
+                                        fill=get_color(weekly[week]))
         hr = ('  <line x1="0" y1="{y}" x2="{x}" y2="{y}" '
               'style="stroke-width: 0.9; stroke: #eee; stroke-opacity: 1;"/>')
-        print >> output, hr.format(x=width - margin * 2 - 10, y=50)
+        print >> output, hr.format(x=main_width - margin * 2 - 10, y=0)
         print >> output, '</g>'
+
+    print >> output, '<g transform="translate({0}, {1})">'.format(
+                      main_width, margin + 10)
+    bar = ('  <rect height="8" width="{height}" x="0" y="{y}" '
+           'style="fill: {fill}"/>')
+    vr = ('  <line x1="0" y1="0" x2="0" y2="{height}" '
+          'style="stroke-width: 0.9; stroke: #eee; stroke-opacity: 1;"/>')
+    maxima = max(wdayly)
+    get_color = _get_bar_color(maxima)
+    for wday in xrange(7):
+        bar_height = 40.0 * wdayly[wday] / maxima
+        print >> output, bar.format(height=bar_height, y = wday * 12 + 2,
+                                    fill=get_color(wdayly[wday]))
+    print >> output, vr.format(height=main_height - 30)
+    print >> output, '</g>'
+
     print >> output, '</svg>'
 
 
